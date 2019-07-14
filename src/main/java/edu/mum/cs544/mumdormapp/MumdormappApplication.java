e34547ae0173e23126d4077bbe077d0cd439f092
sanjayatrital1 <snjtrital@gmail.com>
2019-07-14 15:19:06 -0500

    created starter project

---
+++ src/main/java/edu/mum/cs544/mumdormapp/MumdormappApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs544.mumdormapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumdormappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumdormappApplication.class, args);
+    }
+
+}
